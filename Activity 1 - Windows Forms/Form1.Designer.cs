8b2516f0eb6d7c6ac77a30f7a355bdcea204dca6
ktbloomq <ktb@bloomquist.com>
2022-09-19 14:02:33 -0700

    Add files via upload

---
+++ Activity 1 - Windows Forms/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace Activity_1___Windows_Forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.printText = new System.Windows.Forms.Button();
+            this.clickCount = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // printText
+            // 
+            this.printText.Location = new System.Drawing.Point(250, 71);
+            this.printText.Name = "printText";
+            this.printText.Size = new System.Drawing.Size(87, 23);
+            this.printText.TabIndex = 0;
+            this.printText.Text = "Print Text";
+            this.printText.UseVisualStyleBackColor = true;
+            this.printText.Click += new System.EventHandler(this.printTest_Click);
+            // 
+            // clickCount
+            // 
+            this.clickCount.AutoSize = true;
+            this.clickCount.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.clickCount.Location = new System.Drawing.Point(130, 9);
+            this.clickCount.Name = "clickCount";
+            this.clickCount.Size = new System.Drawing.Size(207, 18);
+            this.clickCount.TabIndex = 1;
+            this.clickCount.Text = "the lope has been clicked 0 times";
+            this.clickCount.Click += new System.EventHandler(this.clickCount_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 72);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(232, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Activity_1___Windows_Forms.Properties.Resources.RunningLope_267;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(112, 57);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(363, 128);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.clickCount);
+            this.Controls.Add(this.printText);
+            this.Name = "Form1";
+            this.Text = "Form Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button printText;
+        private System.Windows.Forms.Label clickCount;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Activity 1 - Windows Forms/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity_1___Windows_Forms
+{
+    public partial class Form1 : Form
+    {
+        // an integer field storing the number of times the clickCount label has been clicked
+        int ClickCounter = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void printTest_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(textBox1.Text);
+        }
+
+        private void clickCount_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            ClickCounter++;
+            clickCount.Text = "The lope has been clicked " + ClickCounter + " times!";
+        }
+    }
+}
